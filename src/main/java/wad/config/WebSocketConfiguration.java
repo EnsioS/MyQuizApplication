7366bf6d6cbef82384685c6636bfa6d9dbfaa46c
Ensio Suonperä <Ensio Suonper>
2017-01-01 23:27:12 +0200

    try to fix heroku problems again

--- src/main/java/wad/config/WebSocketConfiguration.java
+++ src/main/java/wad/config/WebSocketConfiguration.java
@@ -1,27 +1,27 @@
-package wad.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // osoite, johon selain ottaa yhteyttä rekisteröityäkseen
-        registry.addEndpoint("/register").withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // osoite, jonka alla websocket-kommunikaatio tapahtuu
-        registry.setApplicationDestinationPrefixes("/ws");
-        // osoite, jonka kautta viestit kuljetetaan
-        registry.enableSimpleBroker("/channel");
-    }
-
-}
+//package wad.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        // osoite, johon selain ottaa yhteyttä rekisteröityäkseen
+//        registry.addEndpoint("/register").withSockJS();
+//    }
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        // osoite, jonka alla websocket-kommunikaatio tapahtuu
+//        registry.setApplicationDestinationPrefixes("/ws");
+//        // osoite, jonka kautta viestit kuljetetaan
+//        registry.enableSimpleBroker("/channel");
+//    }
+//
+//}
